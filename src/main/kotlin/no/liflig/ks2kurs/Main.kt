195a0a0e28c9e0477d3d5de4bc5fef64a7e10c2b
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2023-02-04 20:42:47 +0100

    feat: add db config

--- src/main/kotlin/no/liflig/ks2kurs/Main.kt
+++ src/main/kotlin/no/liflig/ks2kurs/Main.kt
@@ -1,16 +1,20 @@
 package no.liflig.ks2kurs
 
 import mu.KotlinLogging
+import no.liflig.documentstore.dao.CrudDaoJdbi
 import no.liflig.ks2kurs.common.auth.AuthService
 import no.liflig.ks2kurs.common.auth.DummyAuthService
 import no.liflig.ks2kurs.common.auth.UserPrincipalLens
 import no.liflig.ks2kurs.common.config.Config
+import no.liflig.ks2kurs.common.db.DatabaseConfigurator
+import no.liflig.ks2kurs.common.domain.ServiceRegistry
 import no.liflig.ks2kurs.common.health.HealthService
 import no.liflig.ks2kurs.common.health.createHealthService
 import no.liflig.ks2kurs.common.http4k.ServiceRouter
 import no.liflig.ks2kurs.common.http4k.UserPrincipal
 import no.liflig.ks2kurs.common.http4k.UserPrincipalLog
 import no.liflig.ks2kurs.common.http4k.toLog
+import no.liflig.ks2kurs.services.car.domain.CarRepositoryImpl
 import no.liflig.logging.RequestResponseLog
 import no.liflig.logging.http4k.LoggingFilter
 import org.http4k.core.RequestContexts
@@ -20,8 +24,19 @@ import org.http4k.server.asServer
 
 private val logger = KotlinLogging.logger {}
 
+const val SHOULD_CLEAN_DATABASE_ON_START = true
+
 fun main(args: Array<String>) {
   logger.info { "BuildInfo: ${Config.buildInfo}" }
+
+  val jdbi = DatabaseConfigurator.createJdbiInstanceAndMigrate(
+    DatabaseConfigurator.createDataSource(
+      Config.database.jdbcUrl,
+      Config.database.username,
+      Config.database.password,
+    ),
+    cleanDatabase = SHOULD_CLEAN_DATABASE_ON_START,
+  )
 
   val healthService = createHealthService(Config.applicationName, Config.buildInfo)
 
@@ -35,6 +50,8 @@ fun main(args: Array<String>) {
   val contexts = RequestContexts()
 
   val userPrincipalLens = RequestContextKey.optional<UserPrincipal?>(contexts)
+
+
 
   createApp(
     logHandler = logHandler,
