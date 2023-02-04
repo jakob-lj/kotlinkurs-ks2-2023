9c0feaccf458c70e8587399a5ba1dd54bcf68c48
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2023-02-04 21:32:28 +0100

    feat: add test containers

--- src/test/kotlin/no/liflig/ks2kurs/common/utils/CreateTestApp.kt
+++ src/test/kotlin/no/liflig/ks2kurs/common/utils/CreateTestApp.kt
@@ -1,16 +1,33 @@
 package no.liflig.ks2kurs.common.utils
 
+import no.liflig.documentstore.dao.CrudDaoJdbi
+import no.liflig.ks2kurs.common.domain.ServiceRegistry
 import no.liflig.ks2kurs.common.health.HealthBuildInfo
 import no.liflig.ks2kurs.common.health.HealthService
 import no.liflig.ks2kurs.common.http4k.UserPrincipal
 import no.liflig.ks2kurs.createApp
+import no.liflig.ks2kurs.services.car.domain.CarRepositoryJdbi
+import no.liflig.ks2kurs.services.car.domain.CarSearchRepositoryJdbi
+import no.liflig.ks2kurs.services.car.domain.carSerializerAdapter
 import org.http4k.core.RequestContexts
 import org.http4k.lens.RequestContextKey
 import org.http4k.routing.RoutingHttpHandler
 import java.time.Instant
 
 fun createTestApp(): RoutingHttpHandler {
+  val jdbi = createJdbiForTests(useStaticPort = false)
+
   val contexts = RequestContexts()
+
+  val carRepository = CarRepositoryJdbi(
+    crudDao = CrudDaoJdbi(jdbi, CarRepositoryJdbi.SQL_TABLE_NAME, carSerializerAdapter),
+    searchRepo = CarSearchRepositoryJdbi(jdbi),
+  )
+
+  val serviceRegistry = ServiceRegistry(
+    carRepository = carRepository,
+  )
+
   return createApp(
     logHandler = {},
     requestContexts = contexts,
@@ -24,5 +41,6 @@ fun createTestApp(): RoutingHttpHandler {
         number = 123,
       ),
     ),
+    serviceRegistry = serviceRegistry,
   )
 }
