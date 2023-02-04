b3c84beed1dbcaa2cb0326841eb9772fb903a167
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2023-02-04 22:33:56 +0100

    chore: make car throwable null by default

--- src/main/kotlin/no/liflig/ks2kurs/common/error/CarError.kt
+++ src/main/kotlin/no/liflig/ks2kurs/common/error/CarError.kt
@@ -1,6 +1,6 @@
 package no.liflig.ks2kurs.common.error
 
-class CarError(override val code: CarErrorCode, override val throwable: Throwable?) : ApiError()
+class CarError(override val code: CarErrorCode, override val throwable: Throwable? = null) : ApiError()
 
 enum class CarErrorCode : ApiErrorCode {
   CarNotFound,
