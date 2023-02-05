031b3d06103b65a525de7268b7a827842d6153d5
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2023-02-05 11:25:20 +0100

    chore: use local date serializer in person request

--- src/main/kotlin/no/liflig/ks2kurs/services/person/dtos/CreateOrEditPersonRequest.kt
+++ src/main/kotlin/no/liflig/ks2kurs/services/person/dtos/CreateOrEditPersonRequest.kt
@@ -1,6 +1,10 @@
+@file:UseSerializers(LocalDateSerializer::class)
+
 package no.liflig.ks2kurs.services.person.dtos
 
+import kotlinx.serialization.UseSerializers
 import no.liflig.ks2kurs.common.http4k.lenses.createBodyLens
+import no.liflig.ks2kurs.common.serialization.LocalDateSerializer
 
 @kotlinx.serialization.Serializable
 data class CreateOrEditPersonRequest(
