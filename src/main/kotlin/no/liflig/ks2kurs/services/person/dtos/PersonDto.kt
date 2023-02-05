92ba13763c2ec4f6135bc93abc526d9ea55b6f58
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2023-02-05 18:09:40 +0100

    feat: add birthday to person dto

--- src/main/kotlin/no/liflig/ks2kurs/services/person/dtos/PersonDto.kt
+++ src/main/kotlin/no/liflig/ks2kurs/services/person/dtos/PersonDto.kt
@@ -7,17 +7,20 @@ import no.liflig.ks2kurs.common.http4k.lenses.createBodyLens
 import no.liflig.ks2kurs.common.serialization.LocalDateSerializer
 import no.liflig.ks2kurs.services.person.domain.Person
 import no.liflig.ks2kurs.services.person.domain.PersonId
+import java.time.LocalDate
 
 @kotlinx.serialization.Serializable
 data class PersonDto(
   val id: PersonId,
   val name: String,
+  val birthDay: LocalDate,
 ) {
   companion object {
     val bodyLens by lazy { createBodyLens(serializer()) }
     val example = PersonDto(
       id = PersonId("5831c867-75ab-4b36-9bb1-dfd34b4e58c5"),
       name = "Some name",
+      birthDay = LocalDate.parse("1970-02-01"),
     )
   }
 }
@@ -25,4 +28,5 @@ data class PersonDto(
 fun Person.toDto(): PersonDto = PersonDto(
   id = id,
   name = fullName,
+  birthDay = birthDay,
 )
